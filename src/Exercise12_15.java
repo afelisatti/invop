5aebbf80ebc19593ac2a62a5ce00fae71b5ba174
afelisatti <afelisatti@mulesoft.com>
2016-01-13 23:25:30 -0300

    Starting work on 12.15.

--- src/Exercise12_15.java
+++ src/Exercise12_15.java
@@ -11,9 +11,30 @@ import ilog.cplex.IloCplex;
 public class Exercise12_15 extends Exercise
 {
 
+    public enum Variable {
+        Amount, Started, Watts
+    }
+
     @Override
     public void setUpModel(IloCplex cplex) throws IloException
     {
+        //Set up variables
+        for(int period = 1; period <= 5; period++)
+        {
+            for (int type = 1; type <= 3; type++)
+            {
+                String amountName = getVariableName(Variable.Amount, period, type);
+                setVariable(amountName, cplex.intVar(0, 15, amountName));
+                String startedName = getVariableName(Variable.Started, period, type);
+                setVariable(startedName, cplex.intVar(0, 30, startedName));
+                String wattsName = getVariableName(Variable.Watts, period, type);
+                setVariable(wattsName, cplex.numVar(0.0, Double.MAX_VALUE, wattsName));
+            }
+        }
+    }
 
+    private String getVariableName(Variable variable, int period, int type)
+    {
+        return String.format("%s_Type%s_Period%s", variable, type, period);
     }
 }
