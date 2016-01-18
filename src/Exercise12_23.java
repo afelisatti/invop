b1bc32bb4c6ebd08ff771deab07c0b92495b3400
afelisatti <afelisatti@mulesoft.com>
2016-01-17 23:54:14 -0300

    12.23 done but using constant distances. Need to populate the matrix.

--- src/Exercise12_23.java
+++ src/Exercise12_23.java
@@ -6,21 +6,108 @@
  */
 
 import ilog.concert.IloException;
+import ilog.concert.IloIntVar;
+import ilog.concert.IloLinearNumExpr;
+import ilog.concert.IloObjectiveSense;
 import ilog.cplex.IloCplex;
 
 public class Exercise12_23 extends Exercise
 {
     Double[][] distances = new Double[21][21];
+    int[] demand = new int[]{0, 5, 4, 3, 6, 7, 3, 4, 6, 5, 4, 7, 3, 4, 5, 6, 8, 5, 7, 6, 6};
 
     @Override
     public void setUpModel(IloCplex cplex) throws IloException
     {
         //add variables, constraints and objective
+        IloLinearNumExpr objective = cplex.linearNumExpr();
+
+        for (int day = 1; day <= 2; day++)
+        {
+            IloLinearNumExpr dailyDemand = cplex.linearNumExpr();
+
+            for (int farmFrom = 1; farmFrom <= 21; farmFrom++)
+            {
+                String visitName = getVisit(farmFrom, day);
+                IloIntVar visit = (IloIntVar) setVariable(visitName, cplex.boolVar(visitName));
+
+                dailyDemand.addTerm(demand[farmFrom -1], visit);
+
+                IloLinearNumExpr farmFollows = cplex.linearNumExpr();
+
+                for (int farmTo = 1; farmTo <= 21; farmTo++)
+                {
+                    if (farmFrom != farmTo)
+                    {
+                        String tourName = getTour(day, farmFrom, farmTo);
+                        IloIntVar tour = (IloIntVar) setVariable(tourName, cplex.boolVar(tourName));
+
+                        farmFollows.addTerm(1, tour);
+
+                        objective.addTerm(getDistance(farmFrom, farmTo), tour);
+                    }
+
+                }
+
+                farmFollows.addTerm(-1, visit);
+                cplex.addEq(farmFollows, 0);
+            }
+
+            for (int farmTo = 1; farmTo <= 21; farmTo++)
+            {
+                IloLinearNumExpr farmPrecedes = cplex.linearNumExpr();
+
+                for (int farmFrom = 1; farmFrom <= 21; farmFrom++)
+                {
+                    if (farmFrom != farmTo)
+                    {
+                        farmPrecedes.addTerm(1, getVariable(getTour(day, farmFrom, farmTo)));
+                    }
+                }
+
+                farmPrecedes.addTerm(-1, getVariable(getVisit(farmTo, day)));
+                cplex.addEq(farmPrecedes, 0);
+            }
+
+            cplex.addLe(dailyDemand, 80);
+        }
+
+        for (int farm = 1; farm <= 21; farm++)
+        {
+            IloLinearNumExpr dailyVisit = cplex.linearNumExpr();
+            IloIntVar visit1 = (IloIntVar) getVariable(getVisit(farm, 1));
+            IloIntVar visit2 = (IloIntVar) getVariable(getVisit(farm, 2));
+
+            dailyVisit.addTerm(1, visit1);
+            dailyVisit.addTerm(1, visit2);
+
+            if (farm <= 10)
+            {
+                cplex.addEq(dailyVisit, 2);
+            }
+            else
+            {
+                cplex.addEq(dailyVisit, 1);
+            }
+        }
+
+        cplex.addObjective(IloObjectiveSense.Minimize, objective);
+    }
+
+    private String getTour(int day, int farmTo, int farmFrom)
+    {
+        return String.format("Tour_%s_%s_Day%s", farmTo, farmFrom, day);
+    }
+
+    private String getVisit(int farm, int day)
+    {
+        return String.format("Visit_%s_Day%s", farm, day);
     }
 
     private Double getDistance(int i, int j)
     {
-        return distances[i][j];
+        return 1.0;
+        //return distances[i][j];
     }
 
     private void resolveDistances()
